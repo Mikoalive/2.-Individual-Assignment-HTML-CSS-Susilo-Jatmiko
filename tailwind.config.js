68667a34bd3be80c237310b4bbf81db40d764af0
Mikoalive <mikostillalive@gmail.com>
2023-08-14 20:14:14 +0700

    Individual Assignment HTML CSS Susilo Jatmiko

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["./index.html", "./resume.html", "./contact.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', serif",
+        raleway: "'Raleway', serif",
+      },
+    },
+  },
+  plugins: [],
+}
+
