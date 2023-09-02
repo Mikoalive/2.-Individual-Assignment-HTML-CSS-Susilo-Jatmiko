492880ce6f402a2185c65dc9827dab0cf7bfa5d3
Susilo Jatmiko <127926127+Mikoalive@users.noreply.github.com>
2023-09-02 20:04:34 +0700

    menyimpan file tugas individual assignment kedua

---
+++ main.js
@@ -0,0 +1,11 @@
+let buttonSubmit = document.querySelector("#button-submit");
+buttonSubmit.addEventListener ("click", )
+
+const body = document.querySelector('body');
+const btn = document.getElementById('btn-toggle');
+
+btn.addEventListener('click', () => {
+
+  body.cl
+
+})
